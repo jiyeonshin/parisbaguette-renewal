84357245f1c899cb865482425f6129f8840f969f
jiyeonshin <tlswldus1990@naver.com>
2018-07-15 04:40:31 +0900

    event section 수정

--- docs/js/header.js
+++ docs/js/header.js
@@ -1,8 +1,6 @@
 $('.nav_box>li').hover(function(){
     $(this).find('a').addClass('menu_select');
-    /*$('ul',this).show();    */
     $('.nav_bg').stop().slideDown(200);
-    /*$('.dep2').css('display','block');*/
     $('.dep2').stop().slideDown(200);
 },function(){
     $(this).find('a').removeClass('menu_select');
